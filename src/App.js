92fc2a46ea05117309319eaf8d4638ddcbb79a28
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-24 11:41:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {incNumber, decNumber} from './Redux/actions'
 
 function App() {
+  
+  const countapp = useSelector((state) => state.changeTheNumber);
+    
+  const dispatch = useDispatch()
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h4>hello world</h4>
+         <h2>counter app</h2>
+         <button onClick={() => dispatch(decNumber())}>-</button>
+         
+         <h4><input value={countapp}/></h4>
+         <button onClick={() => dispatch(incNumber())}>+</button>
     </div>
   );
 }
