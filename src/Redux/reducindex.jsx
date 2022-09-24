d6a350c446afce131acec42c5d0399de041e8b95
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-24 11:41:08 +0530

    Redux

---
+++ src/Redux/reducindex.jsx
@@ -0,0 +1,12 @@
+
+import changeTheNumber from "./reducers";
+
+
+import { combineReducers } from "redux";
+
+
+const rootReducer = combineReducers({
+    changeTheNumber
+})
+
+export default rootReducer;
---
+++ src/Redux/store.jsx
@@ -0,0 +1,11 @@
+
+
+import { legacy_createStore as createStore } from "redux";
+
+import rootReducer from "./reducindex";
+
+
+const store = createStore(rootReducer);
+
+
+export default store;
